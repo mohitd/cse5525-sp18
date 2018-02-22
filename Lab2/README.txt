827f38a22445197555f2521db6198cc0e88ffc04
Mohit Deshpande <mohitd2000@gmail.com>
2018-02-22 17:58:19 -0500

    Added Viterbi (min support for unseen words)

---
+++ Lab2/README.txt
@@ -0,0 +1,9 @@
+CSE 5525
+Lab 2
+
+Please run 'python3 util.py' first to generate the transition and emission models from the training data
+
+Commands:
+python3 util.py -- computes transition and emission models (transition_model.pkl and emission_model.pkl, respectively)
+
+python3 part0.py -- computes accuracy of Viterbi decoding trained on counts of training set on the testing set
--- Lab2/part0.py
+++ Lab2/part0.py
@@ -1,38 +1,70 @@
-from util import load_models, load_testing_data
+from util import load_models, load_testing_data, accuracy
 from math import log
 
 def viterbi_decoding(obs, tags, transition_model, emission_model):
-    viterbi = [dict()] * (len(obs) + 1)
-    backpointers = [dict()] * (len(obs) + 1)
+    """Implements the Viterbi algorithm to produce part-of-speech tags
 
-    first_wd = obs[0][0]
+    Arguments:
+        obs {list} -- list of words observation
+        tags {list} -- state space of tags
+        transition_model {dict[dict]} -- transition_model[prev_tag][next_tag]
+        emission_model {dict[dict]} -- emission_model[tag][word]
+
+    Returns:
+        list -- list of predicted tags computed from the Viterbi algorithm
+    """
+    sent_len = len(obs)
+    viterbi = [dict()]
+    backpointers = [dict()]
+
+    # start node
     for tag in tags:
-        viterbi[0][tag] = transition_model['<s>'][tag] + emission_model[tag][first_wd]
+        if obs[0] in emission_model[tag]:
+            p_emission = emission_model[tag][obs[0]]
+        else:
+            p_emission = -1e10
+        viterbi[0][tag] = transition_model['<s>'][tag] + p_emission
         backpointers[0][tag] = 0
 
-    for t in range(1, len(obs)):
-        for tag in tags:
-            viterbi[t][tag] = float('-inf')
-            backpointers[t][tag] = -1
-            p_emission = emission_model[tag][obs[t][0]]
-            for i, tag_prime in enumerate(tags):
-                v = viterbi[t-1][tag_prime] + transition_model[tag_prime][tag] + p_emission
-                if v > viterbi[t][tag]:
-                    viterbi[t][tag] = v
-                    backpointers[t][tag] = i
+    for t in range(1, sent_len):
+        v_t = dict()
+        b_t = dict()
+        for curr_tag in tags:
+            max_score = float('-inf')
+            max_score_idx = -1
+            if obs[t] in emission_model[curr_tag]:
+                p_emission = emission_model[curr_tag][obs[t]]
+            else:
+                p_emission = -1e10
 
-    # last node
-    viterbi[len(obs)] = float('-inf')
-    backpointers[len(obs)] = -1
-    for i, tag_prime in enumerate(tags):
-        v = viterbi[len(obs)-1][tag_prime] + transition_model[tag_prime]['</s>']
-        if v > viterbi[len(obs)]:
-            viterbi[len(obs)] = v
-            backpointers[len(obs)] = i
+            for i, prev_tag in enumerate(tags):
+                score = viterbi[-1][prev_tag] + transition_model[prev_tag][curr_tag] + p_emission
+
+                if score > max_score:
+                    max_score = score
+                    max_score_idx = i
+
+            # end for over s'
+            v_t[curr_tag] = max_score
+            b_t[curr_tag] = max_score_idx
+
+        # end for over s
+        viterbi.append(v_t)
+        backpointers.append(b_t)
+
+    # final node
+    max_score = float('-inf')
+    max_score_idx = -1
+    for i, prev_tag in enumerate(tags):
+        score = viterbi[-1][prev_tag] + transition_model[prev_tag]['</s>']
+        if score > max_score:
+            max_score = score
+            max_score_idx = i
+    backpointers.append(max_score_idx)
 
     backtrace = []
-    idx = backpointers[len(obs)]
-    for t in reversed(range(len(obs))):
+    idx = backpointers[-1]
+    for t in reversed(range(sent_len)):
         backtrace.append(tags[idx])
         idx = backpointers[t][tags[idx]]
 
@@ -42,5 +74,11 @@ def viterbi_decoding(obs, tags, transition_model, emission_model):
 transition_model, emission_model = load_models()
 testing_data = load_testing_data()
 tags = list(transition_model.keys())
+avg_acc = 0.
 
-print(viterbi_decoding(testing_data[0], tags, transition_model, emission_model))
+for example in testing_data:
+    words = list(zip(*example))[0]
+    pred = viterbi_decoding(words, tags, transition_model, emission_model)
+    avg_acc += accuracy(example, pred)
+
+print('Accuracy: {}'.format(avg_acc / len(testing_data)))
